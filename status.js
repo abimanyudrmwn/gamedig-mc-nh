f6ba7a8b70c24a6d78d15aee48331e14a5dc2bb7
A. Nataprawira Darmawan <48431686+abimanyudrmwn@users.noreply.github.com>
2024-05-20 10:10:07 +0700

    Update status.js

--- status.js
+++ status.js
@@ -5,28 +5,15 @@ import fs from 'fs';
 // Replace with your Minecraft server details
 const serverOptions = {
     type: 'minecraft',
-    host: 'your.minecraft.server.ip',
+    host: 'minecraft.belanga.cloud',
     port: 25565  // Default Minecraft port
 };
 
 // Replace with your Discord webhook URL
 const webhookURL = 'https://discord.com/api/webhooks/1241938443223695400/xHD0d6HQZFs81H8IReib54uDn5aCxjmplJIDvxJ9qs3E9vqgz6pcVo0R85852wqLBJzp';
 
-let messageId = null;
-
-const loadMessageId = () => {
-    try {
-        const data = fs.readFileSync('messageId.txt', 'utf8');
-        return data.trim();
-    } catch (err) {
-        console.log('No previous message ID found, starting fresh.');
-        return null;
-    }
-};
-
-const saveMessageId = (id) => {
-    fs.writeFileSync('messageId.txt', id, 'utf8');
-};
+// Static message ID
+const messageId = '1241950512245506093';
 
 const queryServer = async () => {
     try {
@@ -47,47 +34,26 @@ const queryServer = async () => {
             }]
         };
 
-        let response;
-        if (messageId) {
-            response = await fetch(`${webhookURL}/messages/${messageId}`, {
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(message)
-            });
-        } else {
-            response = await fetch(webhookURL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(message)
-            });
-        }
+        const response = await fetch(`${webhookURL}/messages/${messageId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(message)
+        });
 
         const responseText = await response.text();
         console.log('Response Text:', responseText);
 
         if (!response.ok) {
-            throw new Error(`Failed to send or edit webhook: ${response.statusText}`);
+            throw new Error(`Failed to edit webhook message: ${response.statusText}`);
         }
 
-        const responseData = JSON.parse(responseText);
-
-        if (!messageId) {
-            messageId = responseData.id;
-            saveMessageId(messageId);
-        }
-
-        console.log('Webhook operation completed successfully.');
+        console.log('Webhook message updated successfully.');
     } catch (error) {
         console.error('Error querying server or sending webhook:', error);
     }
 };
-
-// Load the message ID from file
-messageId = loadMessageId();
 
 // Schedule the task to run every 5 minutes
 scheduleJob('*/5 * * * *', queryServer);
